e7e84c5663d36992e1a5353cbe7175322a0ef6b1
stephipie <stephi.pietschmann@gmail.com>
2025-03-11 16:41:42 +0100

    add test to add a product to the cart and the whole buying process

--- tests/advanced-test.spec.ts
+++ tests/advanced-test.spec.ts
@@ -46,5 +46,41 @@ test.describe("Wikipedia Playwright article test", () => {
     // Test to save a screenshot if Test fails
     await page.screenshot({ path: "screenshot.png" });
   } );
+
+  test("successful login at saucedemo", async ({ page }) => {
+    // Navigate to the login page  
+    await page.goto("https://www.saucedemo.com/");
+    // Fill in username
+    await page.getByPlaceholder("Username").fill("standard_user");
+     // Fill in password
+    await page.getByPlaceholder("Password").fill("secret_sauce");
+    // Click login button
+    await page.getByRole("button", { name: "Login" }).click();
+    // Verify successful login by checking if we're on the inventory page
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+
+    // Add a product to the cart
+    await page.click("text=Add to cart");
+    // Go to the cart
+    await page.click("text=1");
+    // Check if the product is in the cart
+    const cartItem = await page.locator(".inventory_item_name");
+    await expect(cartItem).toBeTruthy();
+
+    // Go to checkout
+    await page.click("text=Checkout");
+    // Fill in the form
+    await page.fill("input[name=firstName]", "Max");
+    await page.fill("input[name=lastName]", "Mustermann");
+    await page.fill("input[name=postalCode]", "12345");
+    // Click on continue
+    await page.click("text=Continue");
+    // Check if we are on the overview page
+    await expect(page).toHaveURL("https://www.saucedemo.com/checkout-step-two.html");
+    // Finish the order
+    await page.click("text=Finish");
+    // Check if we are on the complete page with the Text "THANK YOU FOR YOUR ORDER"
+    await page.innerHTML("text=THANK YOU FOR YOUR ORDER");
+});
 });
 
